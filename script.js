72adc968a144546b9bcf7b2227d8ec691be4153b
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-07-02 09:39:53 +0100

    Styling finished - JS left

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // Challenge - create error div's in JS alone 
-// Exclamation icon in input field 
+
+'use strict'
+
+const fieldset = document.querySelector('.fieldset')
+const firstName = document.querySelector('#first-name')
+const lastName = document.querySelector('#last-name')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+
+fieldset.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
